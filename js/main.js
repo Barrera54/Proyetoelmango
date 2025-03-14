0e9fc1cd6a6e5778b6ff8e28c55a055319f737d0
Barrera54 <samuelbarrerapatino2403@gmail.com>
2025-03-13 22:03:54 -0500

    Se hizo un cambio en el archivo main.js

--- js/main.js
+++ js/main.js
@@ -1,38 +1,36 @@
 $(document).ready(function(){
 
-	// MOSTRANDO Y OCULTANDO MENU
-	$('#button-menu').click(function(){
-		if($('#button-menu').attr('class') == 'fa fa-bars' ){
+    // MOSTRANDO Y OCULTANDO MENU
+    $('#button-menu').click(function(){
+        if($('.navegacion .menu').css('left') == '-320px' ){ // Verifica si el menú está oculto
 
-			$('.navegacion').css({'width':'100%', 'background':'rgba(0,0,0,.5)'}); // Mostramos al fondo transparente
-			$('#button-menu').removeClass('fa fa-bars').addClass('fa fa-close'); // Agregamos el icono X
-			$('.navegacion .menu').css({'left':'0px'}); // Mostramos el menu
+            $('.navegacion').css({'width':'100%', 'background':'rgba(0,0,0,.5)'}); // Mostramos el fondo transparente
+            $('.navegacion .menu').css({'left':'0px'}); // Mostramos el menu
 
-		} else{
+        } else{
 
-			$('.navegacion').css({'width':'0%', 'background':'rgba(0,0,0,.0)'}); // Ocultamos el fonto transparente
-			$('#button-menu').removeClass('fa fa-close').addClass('fa fa-bars'); // Agregamos el icono del Menu
-			$('.navegacion .submenu').css({'left':'-320px'}); // Ocultamos los submenus
-			$('.navegacion .menu').css({'left':'-320px'}); // Ocultamos el Menu
+            $('.navegacion').css({'width':'0%', 'background':'rgba(0,0,0,.0)'}); // Ocultamos el fondo transparente
+            $('.navegacion .submenu').css({'left':'-320px'}); // Ocultamos los submenus
+            $('.navegacion .menu').css({'left':'-320px'}); // Ocultamos el Menu
 
-		}
-	});
+        }
+    });
 
-	// MOSTRANDO SUBMENU
-	$('.navegacion .menu > .item-submenu a').click(function(){
-		
-		var positionMenu = $(this).parent().attr('menu'); // Buscamos el valor del atributo menu y lo guardamos en una variable
-		console.log(positionMenu); 
+    // MOSTRANDO SUBMENU
+    $('.navegacion .menu > .item-submenu a').click(function(){
 
-		$('.item-submenu[menu='+positionMenu+'] .submenu').css({'left':'0px'}); // Mostramos El submenu correspondiente
+        var positionMenu = $(this).parent().attr('menu'); // Buscamos el valor del atributo menu y lo guardamos en una variable
+        console.log(positionMenu);
 
-	});
+        $('.item-submenu[menu='+positionMenu+'] .submenu').css({'left':'0px'}); // Mostramos El submenu correspondiente
 
-	// OCULTANDO SUBMENU
-	$('.navegacion .submenu li.go-back').click(function(){
+    });
 
-		$(this).parent().css({'left':'-320px'}); // Ocultamos el submenu
+    // OCULTANDO SUBMENU
+    $('.navegacion .submenu li.go-back').click(function(){
 
-	});
+        $(this).parent().css({'left':'-320px'}); // Ocultamos el submenu
+
+    });
 
 });
